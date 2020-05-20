3fc2930d34efe6b2b396330ebd8077df5a183c89
SolvarD <solvar@msn.com>
2020-05-20 20:59:24 +0200

    form admin porte folio

--- Web/ClientApp/src/app/views/admin/porte-folio/admin-porte-folio.component.ts
+++ Web/ClientApp/src/app/views/admin/porte-folio/admin-porte-folio.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ExperienceService } from '../../../services/experience.service';
 import { Experience } from '../../../../models/experience';
 import { BaseComponent } from '../../../../models/base-component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-porte-folio',
@@ -13,36 +14,30 @@ export class AdminPorteFolioComponent extends BaseComponent implements OnInit {
   currentSlide: number = 0;
   slides: Array<Experience> = [];
   displayBloc: boolean[] = [];
+  formsExperiences: FormGroup[] = [];
 
-  constructor(private experienceService: ExperienceService) {
+  constructor(private experienceService: ExperienceService, private _fb: FormBuilder) {
     super();
   }
 
   ngOnInit(): void {
     this.experienceService.getAll().then((res) => {
+
+      res.forEach((experience) => {
+        this.formsExperiences.push(this.buildForm(experience));
+      });
+
       this.slides = res;
     });
   }
 
-  nextSlide() {
-    if (this.currentSlide == (this.slides.length - 1)) {
-      this.currentSlide = 0;
-    } else {
-      this.currentSlide += 1;
-    }
-  }
 
-  previewSlide() {
-    if (this.currentSlide == 0) {
-      this.currentSlide = this.slides.length - 1;
-    }
-    else {
-      this.currentSlide -= 1;
-    }
-  }
-
-  goTo(index: number) {
-    this.currentSlide = index;
+  buildForm(item: Experience) {
+    return this._fb.group({
+      start: [{ value: new Date(item.start.toString()).toLocaleDateString(), disabled: !this.canEdit }, Validators.required],
+      end: [{ value: item.end? new Date(item.end.toString()).toLocaleDateString() : null,disabled: !this.canEdit }, Validators.required],
+      description: [{ value: item.description,disabled: !this.canEdit }, Validators.required]
+    });
   }
 
   display(bloc: number) {
